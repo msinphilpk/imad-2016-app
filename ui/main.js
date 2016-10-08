5c815d628748369a6fe99d050e9fd838edbf13a6
msinphilpk <sinphilpk@gmail.com>
2016-10-08 09:30:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,11 +34,20 @@ var nameIn = document.getElementById('name');
 var name = nameIn.value;
 var submit = document.getElementById('sub_btn');
 submit.onclick = function(){
-  var names = ['name1','name2','name3'];  
-  var list = '';
-  for(i=0;i<names.legth;i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('list');
-  ul.innerHTML = list;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.state === 200){
+                var names = ['name1','name2','name3'];  
+                var list = '';
+                for(i=0;i<names.legth;i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('list');
+                ul.innerHTML = list;
+            }
+        }
+    }
+    request.open('GET','http://msinphilpk.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
 };
