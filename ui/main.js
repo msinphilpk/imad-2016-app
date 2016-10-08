25df5136f2c2a35b7ed5b5eaa47a2f129656279d
msinphilpk <sinphilpk@gmail.com>
2016-10-08 07:10:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,11 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
 var img = document.getElementById("img");
-img.onclick = function(){img.style.marginleft = '100px';};
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight, 100);
+};
