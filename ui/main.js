193bad8d28e340b620874482801307abeb4e4b8c
msinphilpk <sinphilpk@gmail.com>
2016-10-08 07:47:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,16 @@ img.onclick = function(){
 var button = document.getElementById('counter');
 var counter;
 counter.onclick(function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange() = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.state === 200){
+                counter = counter + 1;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString(); 
+            }
+        }
+    };
+    request.open('GET','http://msinphilpk.imad.hasura-app.io/counter',true);
+    request.send(null);
 });
